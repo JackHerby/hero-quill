7e292ae33a09aed9e90575454eaae397fada5d76
Jack Herby <JacekZiolek@proton.me>
2024-10-13 16:12:02 +0200

    add nuxt ui module

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UContainer>
+    <UCard :ui="{ background: 'bg-pink-300 dark:bg-pink-400' }">
+      Hello World
+    </UCard>
+  </UContainer>
 </template>
