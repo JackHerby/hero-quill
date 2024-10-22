c10acc985dac59a8962462793f70c1775c0ccfab
Jack Herby <JacekZiolek@proton.me>
2024-10-22 23:36:38 +0200

    change formatting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   {
-    files: ["**/*.ts", "**/*.js", "**/*.vue"],
+    files: ['**/*.ts', '**/*.js', '**/*.vue'],
     rules: {
-      "@stylistic/max-statements-per-line": ["error", { max: 2 }],
-      "@stylistic/max-len": ["error", { code: 120 }],
+      '@stylistic/max-statements-per-line': ['error', { max: 2 }],
+      '@stylistic/max-len': ['error', { code: 120 }],
     },
   },
-);
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@prisma/nuxt"],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@prisma/nuxt'],
   eslint: {
     config: {
-      stylistic: {
-        semi: true,
-        quotes: "double",
-      },
+      stylistic: true,
     },
   },
-});
+})
