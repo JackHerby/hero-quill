78cd795cde8728a4265cef489b1192b946be7d7e
Jack Herby <JacekZiolek@proton.me>
2024-12-15 23:14:28 +0100

    Add Nuxt UI module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@antfu/eslint-config'],
+  modules: ['@antfu/eslint-config', '@nuxt/ui'],
 })
